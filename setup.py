7f0bb2f45a1f705e5719ad0924686ba49abc6acf
Dyllan Usher <Dyllanjrusher00@gmail.com>
2021-02-03 21:09:31 -0500

    Loaded Data from NSRDB API and started Data Cleaning after loading to HDFS

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is my capstone project following my Big Data Analytics training. I've used Intel's BigDL package which supports Distributed Deep Learning through Apache Spark. You must install a conda environment with Python 3.6, Java JDK, and BigDL.',
+    author='Dyllan Usher',
+    license='',
+)
